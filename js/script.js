0d1a1261fca22a3fa564cf4e3472f26a08fda9de
Bianca Regulski <biancaregulski@yahoo.com>
2020-06-03 17:03:57 -0400

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 const calendar = document.querySelector("#app-calendar");
 const weekDiv = document.querySelector("#day-of-week");
 
+var calBoxColor = getComputedStyle(document.body).getPropertyValue("--color-calendar-box");
+var hoverCalBoxColor = "palegreen";
+
 var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 for (let i = 0; i < days.length; i++) {
-  let el = document.createElement("div");
-  el.className = "day-name";
-  el.appendChild(document.createTextNode(days[i]));
-  weekDiv.appendChild(el);
+  let divElement = document.createElement("div");
+  divElement.className = "day-name";
+  divElement.appendChild(document.createTextNode(days[i]));
+  weekDiv.appendChild(divElement);
 }
 
 for (let i = 1; i < 31; i++) {
-  let el = document.createElement("div");
-  el.className = "day";
-  el.onclick = function () {
-    this.style.backgroundColor = "palegreen";
-};
-  el.appendChild(document.createTextNode(i));
-  calendar.appendChild(el);
+  let pElement =  document.createElement("p");
+  pElement.className = "day-number";
+  pElement.appendChild(document.createTextNode(i));
+
+  let divElement = document.createElement("div");
+  divElement.className = "day";
+  divElement.appendChild(pElement);
+
+  divElement.onclick = function () {
+    if (this.style.backgroundColor == hoverCalBoxColor) {
+      this.style.backgroundColor = calBoxColor;
+    }
+    else {
+      this.style.backgroundColor = hoverCalBoxColor;
+    }
+  };
+  
+  calendar.appendChild(divElement);
 }
