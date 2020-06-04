bebda16d7f3a9c23aac46e953b07f40513c7dacd
Bianca Regulski <biancaregulski@yahoo.com>
2020-06-03 21:28:10 -0400

    Update layout

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+class Day {
+  constructor() {
+    this.selected = false;
+  }
+}
+
 const calendar = document.querySelector("#app-calendar");
 const weekDiv = document.querySelector("#day-of-week");
 
-var calBoxColor = getComputedStyle(document.body).getPropertyValue("--color-calendar-box");
-var hoverCalBoxColor = "palegreen";
+var defaultBoxColor = getComputedStyle(document.body).getPropertyValue("--color-calendar-box");
+var selectedBoxColor = getComputedStyle(document.body).getPropertyValue("--color-calendar-box-hover");
 
-var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var days = [];
+var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-for (let i = 0; i < days.length; i++) {
+for (let i = 0; i < daysOfWeek.length; i++) {
   let divElement = document.createElement("div");
   divElement.className = "day-name";
-  divElement.appendChild(document.createTextNode(days[i]));
+  divElement.appendChild(document.createTextNode(daysOfWeek[i]));
   weekDiv.appendChild(divElement);
 }
 
@@ -20,15 +27,32 @@ for (let i = 1; i < 31; i++) {
 
   let divElement = document.createElement("div");
   divElement.className = "day";
+  divElement.dataset.num = i;
   divElement.appendChild(pElement);
 
+  // create Day object
+  let newDay = new Day();
+  days.push(newDay);
+  
+  // when box is clicked, check if box is selected
   divElement.onclick = function () {
-    if (this.style.backgroundColor == hoverCalBoxColor) {
-      this.style.backgroundColor = calBoxColor;
+    let index = parseInt(this.dataset.num) - 1;
+    
+    if (days[index].selected == true) {
+      days[index].selected = false;
+      this.style.backgroundColor = defaultBoxColor;
     }
     else {
-      this.style.backgroundColor = hoverCalBoxColor;
+      days[index].selected = true;
+      this.style.backgroundColor = selectedBoxColor;
     }
+/*
+    if (this.style.backgroundColor == selectedBoxColor) {
+      this.style.backgroundColor = defaultBoxColor;
+    }
+    else {
+      this.style.backgroundColor = selectedBoxColor;
+    }*/
   };
   
   calendar.appendChild(divElement);
